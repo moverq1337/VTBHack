5aaa7d028bb47811738b6d0cad6418a17433d505
moverq1337 <moverq1337@gmail.com>
2025-09-04 00:42:02 +0300

    commit w8 4 unk

---
+++ internal/models/models.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Vacancy struct {
+	ID               uuid.UUID `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	Title            string    `gorm:"type:varchar(255)"`
+	Requirements     string    `gorm:"type:text"`
+	Responsibilities string    `gorm:"type:text"`
+	CreatedAt        time.Time
+}
+
+type Resume struct {
+	ID          uuid.UUID `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	CandidateID uuid.UUID `gorm:"type:uuid"`
+	Text        string    `gorm:"type:text"`
+	ParsedData  string    `gorm:"type:jsonb"` // JSONB для хранения парсированных данных
+	CreatedAt   time.Time
+}
+
+type Interview struct {
+	ID          uuid.UUID `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	CandidateID uuid.UUID `gorm:"type:uuid"`
+	Transcript  string    `gorm:"type:text"`
+	Score       float64
+	Report      string `gorm:"type:jsonb"`
+	CreatedAt   time.Time
+}
---
+++ scripts/migrate.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/moverq1337/VTBHack/internal/config"
+	"github.com/moverq1337/VTBHack/internal/db"
+	"github.com/moverq1337/VTBHack/internal/models"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbConn, err := db.Connect(cfg.DBURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = dbConn.AutoMigrate(&models.Vacancy{}, &models.Resume{}, &models.Interview{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Миграции завершены")
+}
